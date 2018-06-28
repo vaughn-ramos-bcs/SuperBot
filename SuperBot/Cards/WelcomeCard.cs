858deb1b860637e915c98691c58014bdaf0378d5
vaughn-ramos-bcs <Vaughn.Ramos@bcstechnology.com.au>
2018-06-28 08:43:22 +0800

    Restructured files and separated concerns:
    -Welcome Card has now been applied using Adapter Card
    -OnboardMe module has been transferred to a different Dialog class

---
+++ SuperBot/Cards/WelcomeCard.cs
@@ -0,0 +1,68 @@
+﻿using AdaptiveCards;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperBot.Cards
+{
+    public class WelcomeCard
+    {
+        public static Attachment Welcome()
+        {
+            AdaptiveCard adaptiveCards = new AdaptiveCard()
+            {
+                Body = new List<AdaptiveElement>()
+                {
+                    new AdaptiveTextBlock()
+                    {
+                        Text = "Hi! \U0001F60A Welcome to the Super Bot. I'll assist you in every way I can. \U00002708 What would you like to do?",
+                        Wrap = true
+                    }
+                },
+                Actions = new List<AdaptiveAction>()
+                {
+                    new AdaptiveSubmitAction()
+                    {
+                        Title = "Ask HR",
+                        Data = "Ask HR"
+                    },
+                    new AdaptiveSubmitAction()
+                    {
+                        Title = "Contact IT",
+                        Data = "Contact IT"
+                    },
+                    new AdaptiveSubmitAction()
+                    {
+                        Title ="Contact Graphic Team",
+                        Data = "Contact Graphic Team"
+                    },
+                    new AdaptiveSubmitAction()
+                    {
+                        Title = "File Leave",
+                        Data = "File Leave"
+                    },
+                    new AdaptiveSubmitAction()
+                    {
+                        Title = "Get Payslip",
+                        Data = "Get Payslip"
+                    },
+                    new AdaptiveSubmitAction()
+                    {
+                        Title = "Onboard Me",
+                        Data = "Onboard Me"
+                    }
+                }
+            };
+
+            Attachment attachment = new Attachment()
+            {
+                ContentType = AdaptiveCard.ContentType,
+                Content = adaptiveCards
+            };
+
+            return attachment;
+        }
+    }
+}
---
+++ SuperBot/Services/DialogHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using SuperBot.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SuperBot.Services
+{
+    public class DialogHelper
+    {
+        public static async Task Check(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var activity = await result as Activity;
+            var message = activity.Text.ToString().ToLower();
+
+            if (message.Contains("restart") || message.Contains("stop") || message.Contains("go back"))
+            {
+                var compose = context.MakeMessage();
+                compose.Attachments.Add(WelcomeCard.Welcome());
+
+                await context.PostAsync(compose, CancellationToken.None);
+                
+            }
+        }
+    }
+}
